4bb05057a3c5b04567d51bdd78840335fb552172
Ngo Tuan Anh <ngotuananh2101@gmmail.com>
2026-03-22 02:31:36 +0700

    fix: force HTTPS scheme for URLs and update progress bar color
    - Add conditional logic to force HTTPS scheme if app URL starts with 'https://'
    - Change progress bar color to a more noticeable red (#c12222)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->configureDefaults();
+
+        if (str_starts_with(config('app.url', ''), 'https://')) {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
     }
 
     /**
